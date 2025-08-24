57a587b854ef99081ce9d85381b90626e97ad661
rohit singh jagawat <rohtjagawat45@gmail.com>
2025-08-24 23:07:57 +0530

    Day 1: JavaScript basics and student object task

---
+++ Day1.js
@@ -0,0 +1,13 @@
+// student.js
+const student = {
+  name: "Rohit",
+  age: 21,
+  course: "Computer Science",
+  college: "Mandsaur University"
+};
+
+console.log("Student Details:");
+console.log("Name:", student.name);
+console.log("Age:", student.age);
+console.log("Course:", student.course);
+console.log("College:", student.college);
